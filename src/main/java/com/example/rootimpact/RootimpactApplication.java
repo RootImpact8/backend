42cf38424cbd0be5301daf5d9d1404a06b236ad5
maeng555 <mjy5188@naver.com>
2025-01-30 21:47:17 +0900

    first commit

---
+++ src/main/java/com/example/rootimpact/RootimpactApplication.java
@@ -0,0 +1,13 @@
+package com.example.rootimpact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RootimpactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RootimpactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rootimpact/RootimpactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rootimpact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RootimpactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
